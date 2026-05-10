a472ebee4fa2165307de37dfb08c78715c241197
rushi <rushi@example.com>
2026-05-10 16:36:14 +0530

    save

--- frontend/web/remove_linkedin.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('index.html', 'utf8');
-
-// Replace the linkedin icon div block across the file
-const replaced = data.replace(/\s*<div>\s*<a href="#" class="tc-social"><i class="fa-brands fa-linkedin-in"><\/i><\/a>\s*<\/div>/g, '');
-
-fs.writeFileSync('index.html', replaced);
-console.log('LinkedIn icons removed successfully.');
